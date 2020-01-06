7aee2dfa642de90ffa9afa1e5c1ac350fb3b5340
Jorge <jorge.plaza@usach.cl>
2020-01-06 19:01:09 -0300

    Se agrega entradas de usuario

--- frutas.py
+++ frutas.py
@@ -2,7 +2,16 @@ import skfuzzy as fuzz
 import matplotlib.pyplot as plt
 import numpy as np
 
+def entradaUsuario():
+	altura = input('Ingrese la altura de la Cereza (mm): ')
+	diametro = input('Ingrese el diametro del fruto (mm): ')
+	transparencia = input('Ingrese la transparencia del fruto (%): ')
+	cobertura = input('Ingrese la cobertura de las manchas del fruto (%): ')
+	return altura, diametro, transparencia, cobertura
+
+
 def main():
 	print('Hola Mundo')
+	altura, diametro, transparencia, cobertura = entradaUsuario()
 
 main()
