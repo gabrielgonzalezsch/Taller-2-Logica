2e60266b72ab8e01b1b6311832c1dbca14cda878
Unknown <jorge.plaza@usach.cl>
2020-01-15 11:12:07 -0300

    Se agregan comentarios

--- frutas.py
+++ frutas.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from skfuzzy import control as ctrl
 
+# Funcion que define las reglas del sistema descritas en el enunciado del Taller
+# Entrada: La forma, firmeza, cobertura y comercializacion
+# Retorna las reglas en forma de array
 def reglas(forma, firmeza, cobertura, comercializacion):
 	regla1 = ctrl.Rule(forma['Angosta'] & firmeza['Verde'] & cobertura['Leve'], comercializacion['Exportable'])
 	regla2 = ctrl.Rule(forma['Angosta'] & firmeza['Verde'] & cobertura['Parcial'], comercializacion['Comercial'])
@@ -33,6 +36,9 @@ def reglas(forma, firmeza, cobertura, comercializacion):
 	regla27 = ctrl.Rule(forma['Ancha'] & firmeza['Podrida'] & cobertura['Completa'], comercializacion['Desecho'])
 	return [regla1, regla2, regla3, regla4, regla5, regla6, regla7, regla8, regla9, regla10, regla11, regla12, regla13, regla14, regla15, regla16, regla17, regla18, regla19, regla20, regla21, regla22, regla23, regla24, regla25, regla26, regla27] 
 
+# Funcion que grafica segun parametros de entrada
+# Recibe los valores de x, y los nombres de los datos,
+# los nombres de x e y y el titulo del grafico
 def graficar(x, y, nombres, xlabel, ylabel, title):
 	plt.figure(title)
 	cont = 0
@@ -45,6 +51,8 @@ def graficar(x, y, nombres, xlabel, ylabel, title):
 	plt.title(title)
 	return
 
+# Funcion que crea la funcion de pertenencia de la cobertura del fruto
+# y la devuelve, llamando a la funcion grafica antes
 def coberturaFruto():
 	cobertura = ctrl.Antecedent(np.arange(0, 100), 'Cobertura')
 	cobertura['Leve'] = coberturaLeve(cobertura)
@@ -53,6 +61,8 @@ def coberturaFruto():
 	graficar(cobertura.universe, [cobertura['Leve'].mf, cobertura['Parcial'].mf, cobertura['Completa'].mf], ['Leve', 'Parcial', 'Completa'], "Cobertura del fruto (%)", "Grado de pertenencia", "Cobertura")
 	return cobertura
 
+# Funcion que crea la funcion de pertenencia de la firmeza del fruto
+# y la devuelve, llamando a la funcion graficar antes
 def firmezaDelFruto():
 	firmeza = ctrl.Antecedent(np.arange(0, 100), 'Firmeza')
 	firmeza['Verde'] = frutaVerde(firmeza)
@@ -61,6 +71,8 @@ def firmezaDelFruto():
 	graficar(firmeza.universe, [firmeza['Verde'].mf, firmeza['Madura'].mf, firmeza['Podrida'].mf], ['Verde', 'Madura', 'Podrida'], "Firmeza del fruto (%)", "Grado de pertenencia", "Firmeza")
 	return firmeza
 
+# Funcion que crea la funcion de pertenencia de la forma del fruto
+# y la devuelve, llamando a la funcion graficar antes
 def formaFruto():
 	forma = ctrl.Antecedent(np.arange(0, 3), 'Forma')
 	forma['Angosta'] = frutaAngosta(forma)
@@ -69,6 +81,8 @@ def formaFruto():
 	graficar(forma.universe, [forma['Angosta'].mf, forma['Normal'].mf, forma['Ancha'].mf], ['Angosta', 'Normal', 'Ancha'], "Forma del fruto (mm)", "Grado de pertenencia", "Forma")
 	return forma
 
+# Funcion que crea la funcion de pertenencia de la comercializacion del fruto
+# y la devuelve, llamando a la funcion graficar antes
 def comercializacion():
 	comercializacion = ctrl.Consequent(np.arange(0, 100), 'Comercializacion')
 	comercializacion['Desecho'] = fuzz.trapmf(comercializacion.universe, [0, 0, 15, 45])
@@ -77,51 +91,70 @@ def comercializacion():
 	graficar(comercializacion.universe, [comercializacion['Desecho'].mf, comercializacion['Comercial'].mf, comercializacion['Exportable'].mf], ['Desecho', 'Comercial', 'Exportable'], "Calidad del fruto (%)", "Grado de pertenencia", "Calidad")
 	return comercializacion
 
+# Funcion que define el rango de pertenencia de la fruta
+# cuando es angosta, devolviendo el rango
 def frutaAngosta(forma):
 	# Funcion de pertenencia para forma de angosta de fruta
 	angosta = fuzz.trapmf(forma.universe, [0, 0, 0.5, 1])
 	return angosta
 
+# Funcion que define el rango de pertenencia de la fruta
+# cuando es normal, devolviendo el rango
 def frutaNormal(forma):
 	# Funcion de pertenencia para forma de normal de fruta
 	normal = fuzz.trimf(forma.universe, [0.5, 1, 1.5])
 	return normal
 
+# Funcion que define el rango de pertenencia de la fruta
+# cuando es ancha, devolviendo el rango
 def frutaAncha(forma):
 	# Funcion de pertenencia para forma ancha de fruta
 	ancha = fuzz.trapmf(forma.universe, [1.5, 2, 3, 3])
 	return ancha
 
+# Funcion que define el rango de pertenencia de la fruta
+# cuando tiene cobertura leve, devolviendo el rango
 def coberturaLeve(cobertura):
 	# Funcion de pertenencia para cobertura de manchas leve
 	leve = fuzz.trapmf(cobertura.universe, [0, 0, 15, 45])
 	return leve
 
+# Funcion que define el rango de pertenencia de la fruta
+# cuando tiene cobertura parcial, devolviendo el rango
 def coberturaParcial(cobertura):
 	# Funcion de pertenencia para cobertura de manchas parcial
 	parcial = fuzz.trimf(cobertura.universe, [15, 45, 75])
 	return parcial
 
+# Funcion que define el rango de pertenencia de la fruta
+# cuando tiene cobertura completa, devolviendo el rango
 def coberturaCompleta(cobertura):
 	# Funcion de pertenencia para cobertura de manchas completa
 	completa = fuzz.trapmf(cobertura.universe, [45, 75, 100, 100])
 	return completa
 
+# Funcion que define el rango de pertenencia de la fruta
+# es verde, devolviendo el rango
 def frutaVerde(firmeza):
 	# Funcion de pertenencia para fruta verde
 	verde = fuzz.trapmf(firmeza.universe, [0, 0, 15, 45])
 	return verde
 
+# Funcion que define el rango de pertenencia de la fruta
+# es madura, devolviendo el rango
 def frutaMadura(firmeza):
 	# Funcion de pertenencia para fruta madura
 	madura = fuzz.trimf(firmeza.universe, [15, 45, 75])
 	return madura
 
+# Funcion que define el rango de pertenencia de la fruta
+# esta podrida, devolviendo el rango
 def frutaPodrida(firmeza):
 	# Funcion de pertenencia para fruta podrida
 	podrida = fuzz.trapmf(firmeza.universe, [45, 75, 100, 100])
 	return podrida
 
+# Funcion que pide las entradas al usuario.
 def entradaUsuario():
 
 	while True:
@@ -155,6 +188,8 @@ def entradaUsuario():
 
 	return altura, diametro, transparencia, cobertura
 
+# Funcion que devuelve la salida de embalaje en base
+# a las entradas del diametro y el comercio del fruto
 def salidaEmbalaje(diametro, comercio):
 	if(comercio == "Desecho"):
 		return 16
@@ -189,6 +224,8 @@ def salidaEmbalaje(diametro, comercio):
 	elif(diametro == 32):
 		return 15
 
+# Funcion que recibe los valores de cada antecedente y consecuente e imprime
+# los valores finales en el archivo de salida
 def escribirArchivoSalida(resultadoComercio, altura, diametro, transparencia, cobertura):
 	
 	nombreArchivo = "Cereza_"+str(altura)+"_"+str(diametro)+"_"+str(transparencia)+"_"+str(cobertura)+".txt"
@@ -231,6 +268,9 @@ def escribirArchivoSalida(resultadoComercio, altura, diametro, transparencia, co
 	salida = salidaEmbalaje(diametro, comercio)
 	archivo.write("Numero salida: "+str(salida))
 
+# Funcion principal del programa, en el se llama a las funciones para
+# ejecutar las funcionalidades, finalmente muestra los resultados finales
+# de los graficos de pertenencia
 def main():
 	altura, diametro, transparencia, cobertura = entradaUsuario()
 	firmezaAntecedente = firmezaDelFruto()
@@ -257,4 +297,5 @@ def main():
 
 	plt.show()
 
+# Se llama a la funcion main para comenzar con la ejecucion del programa
 main()
